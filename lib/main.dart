0941edc8630e8d7e33dff2add25db9310ffc10b6
SharpShooter263 <caglar26362@gmail.com>
2025-11-16 07:13:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text("MySphereClean")),
-      body: Center(child: Text("Hello World")),
-    ),
-  ));
+  runApp(const MySphereApp());
+}
+
+class MySphereApp extends StatelessWidget {
+  const MySphereApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MySphere',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+        primaryColor: const Color(0xFF3F51B5),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.indigo,
+        ),
+        scaffoldBackgroundColor: const Color(0xFFF7F5FF),
+        fontFamily: 'Roboto',
+      ),
+      home: const LoginScreen(),
+    );
+  }
 }
