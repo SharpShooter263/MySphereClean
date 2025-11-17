e028d7b50fee5b7f813a3742136570f2f900c3c5
SharpShooter263 <caglar26362@gmail.com>
2025-11-17 06:34:06 +0300

    Update register_screen.dart

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -89,6 +89,14 @@ class _RegisterScreenState extends State<RegisterScreen> {
   }
 
   @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF3EFFC),
